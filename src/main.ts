4822371d8e3dcd9f60c20d879e47f61ed5a3c25f
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-31 00:06:37 -0600

    clear main.ts for types

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,3 @@
-type ID = string
-type PopulatTag = string
-
-interface UserInterface {
-  id: ID
-  name: string,
-  surname: string,
-  age?: number,
+const doSomething = (): void => {
+  console.log('doSomething')
 }
-
-
-
-const user: UserInterface | null = null
-
-
-const user2: UserInterface = {
-  name: 'Jack',
-  surname: 'Smith'
-}
-
-
-let unionUsage: number | string = '1'
-
-let errorMessage: string | null = null
-
