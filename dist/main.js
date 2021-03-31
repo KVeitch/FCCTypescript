5d516e52a56d29428623a45f678902de4ee9c75d
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-30 23:58:16 -0600

    Compile main.js

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,8 @@
-var a = "ll";
-console.log('aaa', a);
+var popTag = ['typescript', 'react'];
+var user = null;
+var user2 = {
+    name: 'Jack',
+    surname: 'Smith'
+};
+var unionUsage = '1';
+var errorMessage = null;
