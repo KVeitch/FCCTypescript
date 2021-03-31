af48d907ca7528ed83cf917af9b1a23b06a77237
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-30 23:55:13 -0600

    Add tyoes to main.js

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
-const a = "ll"
-console.log('aaa', a)
+type ID = string
+type PopulatTag = string
+
+interface UserInterface {
+  id: ID
+  name: string,
+  surname: string,
+  age?: number,
+}
+
+
+
+const user: UserInterface | null = null
+
+
+const user2: UserInterface = {
+  name: 'Jack',
+  surname: 'Smith'
+}
+
+
+let unionUsage: number | string = '1'
+
+let errorMessage: string | null = null
+
